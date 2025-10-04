337df7dce2209ab53ae51e26a9bb70b47a422b37
Vand0525 <vand0525@algonquinlive.com>
2025-10-04 15:00:14 -0400

    added cors please work

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 dotenv.config();
 import connect from './config/db.js';
 import articlesRoutes from './routes/articles.js';
@@ -8,6 +9,11 @@ connect();
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 app.use('/articles', articlesRoutes);
 
